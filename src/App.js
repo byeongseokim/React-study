15e1d5d5cea3e581e77b06c3843a5d44b8c0ca5b
byeongseokim <kbs970318@gmil.com>
2023-05-31 13:15:58 +0900

    컴포넌트 만들기
    props

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+
+function Header(props) {
+  return (
+  <header>
+      <h1><a href="/">{props.title}</a></h1>
+      </header>
+  )
+}
+
+function App() {
+  const topics = [
+{id:1, title:'html', body:'html is ...'},
+{id:2, title:'css', body:'css is ...'},
+{id:3, title:'javascript', body:'javascript is ...'}
+  ]
+  return (
+    <div>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+        <Article title="Welcome" body="Hello, WEB"></Article>
+        </div>
+  )
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return (
+    <nav>
+    <ol>
+      {lis}
+    </ol>
+    </nav>
+  )
+}
+
+
+function Article(props) {
+  return (
+    <article>
+          <h2>{props.title}</h2>
+          {props.body}
+        </article>
+  );
+}
+export default App;
