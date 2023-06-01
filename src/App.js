2ea89c931b60d7c28030c062f73796d546a56c58
byeongseokim <kbs970318@gmil.com>
2023-06-01 13:03:58 +0900

    이벤트
    onChangeMode / onclick

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import './App.css';
 
 function Header(props) {
+  console.log("props", props.title)
   return (
   <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
       </header>
   )
 }
 
 function App() {
+  const mode =  'READ';
   const topics = [
 {id:1, title:'html', body:'html is ...'},
 {id:2, title:'css', body:'css is ...'},
 {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, Web"></Article>
+  } else if (mode === 'READ') {
+    content = <Article title="Welcome" body="Hello, Read"></Article>
+  }
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
-        <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        mode = 'WELCOME';
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        mode ='READ'
+        }}></Nav>
+        {content}
         </div>
   )
 }
@@ -27,7 +42,12 @@ function Nav(props) {
   const lis = []
   for(let i=0; i<props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+      </li>);
   }
   return (
     <nav>
