7f568f361166007f8919ecee254c2b4de58b3820
byeongseokim <kbs970318@gmil.com>
2023-06-07 10:37:24 +0900

    글 추가한 후 에 추가된 글로 이동하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -54,26 +54,32 @@ function Article(props) {
   );
 }
 
-function Create() {
+function Create(props) {
   return <article>
       <h2>Create</h2>
-      <form>
+      <form onSubmit={event=>{
+        event.preventDefault();;
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
         <p><input type="text" name="title" placeholder="title" /></p>
         <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
       </form>
     </article>
 }
 
 function App() {
   const [mode, setMode] = useState("WELCOME");
-
   const [id, setId] = useState(null);
-  //  console.log('_mode', _mode);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
-  ];
+  ]);
+  
   let content = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>;
@@ -89,7 +95,15 @@ function App() {
     }
     content = <Article title={title} body={body}></Article>;
   } else if (mode === "CREATE") {
-    content = <Create></Create>
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
