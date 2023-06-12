675846717789ab78cf86b02047abe5b66c18444d
byeongseokim <kbs970318@gmil.com>
2023-06-12 15:54:17 +0900

    Context API

--- context-api-example/src/App.js
+++ context-api-example/src/App.js
@@ -1,36 +1,49 @@
-import "./App.css";
+import "./style.css";
+import React, { createContext, useContext } from "react";
+
+const themeDefault = { border: "10px solid lightpink" };
+const themeContext = createContext(themeDefault);
 
 function App() {
+  const theme = useContext(themeContext);
+  console.log("theme", theme);
   return (
-    <div className="root">
-      <h1>Hello World!</h1>
-      <Sub1 />
-    </div>
+    <themeContext.Provider value={{ border: "10px solid lightskyblue" }}>
+      <div className="root" style={theme}>
+        <h1>Hello World!</h1>
+        <Sub1 />
+      </div>
+    </themeContext.Provider>
   );
 }
 
-function Sub1(){
+function Sub1() {
+  const theme = useContext(themeContext);
   return (
-  <div>
-    <h1>Sub1</h1>
-    <Sub2 />
-  </div>
+    <themeContext.Provider value={{ border: "10px solid lightgreen" }}>
+      <div style={theme}>
+        <h1>Sub1</h1>
+        <Sub2 />
+      </div>
+    </themeContext.Provider>
   );
 }
 
 function Sub2() {
+  const theme = useContext(themeContext);
   return (
-    <div>
+    <div style={theme}>
       <h1>Sub2</h1>
       <Sub3 />
     </div>
   );
 }
 
-function Sub3() {
+function Sub3() { 
+  const theme = useContext(themeContext);
   return (
-    <div>
-      <h1>Sub2</h1>
+    <div style={theme}>
+      <h1>Sub3</h1>
     </div>
   );
 }
