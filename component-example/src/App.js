e4b2f5e8df30f0b7c036cf0a157cdededc1af71d
byeongseokim <kbs970318@gmil.com>
2023-06-14 12:21:16 +0900

    클래스형 컴포넌트

---
+++ component-example/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
+}
+
+export default App;
