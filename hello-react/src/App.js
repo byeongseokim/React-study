c7bf1a2fd328611239146d7e2eb4edc9dfbd47e5
byeongseokim <kbs970318@gmil.com>
2023-06-14 12:20:10 +0900

    ESLint 와 Prettier적용
    환경설정

---
+++ hello-react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  const name = "리액트";
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
+  );
+}
+
+export default App;
