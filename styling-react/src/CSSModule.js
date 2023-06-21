45d07b14dd30ed433d788dcccbecdc0f16a8f703
byeongseokim <kbs970318@gmil.com>
2023-06-21 13:02:22 +0900

    styling2

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,10 @@
+import styles from "./CSSModule.module.scss";
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
