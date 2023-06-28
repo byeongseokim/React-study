283192684dd123b6dc512496312aaacefe85ffc7
byeongseokim <kbs970318@gmil.com>
2023-06-28 14:06:53 +0900

    API

---
+++ news-viewer/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=edab471c9ce143f8b975503e48e25294',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
