6e95ae2d1873b538e591fddc1187f69dbdcddd2a
byeongseokim <kbs970318@gmil.com>
2023-07-04 15:42:51 +0900

    미들웨어

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./modules";
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
